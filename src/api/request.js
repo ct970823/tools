971054ef60d139cff8124268984556a94cb2ed22
ct970823 <275493164@qq.com>
2021-04-09 15:59:52 +0800

    react工具

--- src/api/request.js
+++ src/api/request.js
@@ -51,33 +51,3 @@ const request = extend({
 });
 
 export default request;
-
-
-
-
-
-
-
-
-// export default function ajax(url,data={},method='GET',config={}) {
-//     return new Promise((resolve,reject) => {
-//         let promise
-//         // 1. 执行异步ajax请求
-//         if(method === 'GET'){
-//             promise = axios.get(url, {
-//                 params:data
-//             })
-//         }else{
-//             promise = axios.post(url,data)
-//         }
-//         promise.then(response => {
-//             // 2. 成功调用resolve
-//             resolve(response.data)
-//         }).catch(error => {
-//             // 3. 失败，不调用reject ，直接提示异常信息
-//             message.error('请求出错：'+error.message)
-//         })
-//
-//     })
-//
-// }
